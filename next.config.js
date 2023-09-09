a1ba9a828db05a775e6e23f84ca3110ade297f14
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-09-09 13:23:10 -0400

    List pdf-parse as server comp package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["pdf-parse"],
+  },
 }
 
 module.exports = nextConfig
